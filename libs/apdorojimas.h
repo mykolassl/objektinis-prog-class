ac3c02c90069d550676fc2a7eeb459b635f0ab63
mykolassl <emykolas@gmail.com>
2023-04-16 15:47:04 +0300

    Prideti du dalinimo budai

--- libs/apdorojimas.h
+++ libs/apdorojimas.h
@@ -8,3 +8,5 @@ void isvesti_faila(vector<Studentas>& , string );
 void generuoti_failus();
 void pildyti(Studentas& , bool& , int );
 void spausdinti(const Studentas );
+void dalinimas_1(vector<Studentas>& , vector<Studentas>& , vector<Studentas>& );
+void dalinimas_2(vector<Studentas>& , vector<Studentas>& );
