f9b7903cba9815e020d4ca78c53b02100733811c
mykolassl <emykolas@gmail.com>
2023-05-12 11:34:28 +0300

    Prideti testavimo atvejai

--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,20 @@
 #include "gtest/gtest.h"
+#include "pagalbines_funk.h"
+#include "apdorojimas.h"
+#include <cmath>
 
-TEST(expect, expect__Test) {
-    EXPECT_EQ(2*2, 4);
+TEST(GalutinioBaloSkaiciavimas, GalutinisVidurkis) {
+    vector<int> pazymiai_1 = {4, 8, 10, 6, 8, 7};
+    vector<int> pazymiai_2 = {1, 2, 5, 4, 3, 8};
+
+    EXPECT_EQ(7.17, round(apskaiciuoti_vidurki(pazymiai_1) * 100.0) / 100.0);
+    EXPECT_FALSE(apskaiciuoti_vidurki(pazymiai_2) == 2.5);
+}
+
+TEST(GalutinioBaloSkaiciavimas, GalutinisMediana) {
+    vector<int> pazymiai_1 = {5, 8, 10, 6, 8, 7};
+    vector<int> pazymiai_2 = {1, 2, 5, 4, 3, 8};
+
+    EXPECT_EQ(7.5, apskaiciuoti_mediana(pazymiai_1));
+    EXPECT_FALSE(apskaiciuoti_mediana(pazymiai_2) == 8);
 }
